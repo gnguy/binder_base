c1b409198e81e34dce25cc413c1326029062a136
gnguy <grant.nguyen@gmail.com>
2019-10-07 14:05:55 -0700

    Add more sf apt-get dependencies

--- install.R
+++ install.R
@@ -2,7 +2,7 @@ install_packages <- function(package_name) {
   if(!package_name %in% installed.packages()) install.packages(package_name)
 }
 
-package_list <- c("tidyverse", "readr", "dplyr", "shiny", "shinytemplate", "plotly", "data.table",
+package_list <- c("tidyverse", "readr", "dplyr", "shiny", "shinythemes", "plotly", "data.table",
                   "xgboost", "lme4", "Hmisc", "htmlwidgets", "openxlsx", "DT",
                   "rmarkdown", "leaflet", "lubridate", "sf", "bit64", "IRkernel")
 
