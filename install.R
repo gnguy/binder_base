c6f5e456bad9fb6fee182c2fd412fadf1be9053f
gnguy <grant.nguyen@gmail.com>
2019-10-07 13:21:33 -0700

    Fix install and add apt-get for udunits error

--- install.R
+++ install.R
@@ -2,11 +2,8 @@ install_packages <- function(package_name) {
   if(!package_name %in% installed.packages()) install.packages(package_name)
 }
 
-package_list <- c("tidyverse", "readr", "dplyr", "shiny", "shinytemplates", "plotly", "data.table",
-                  "xgboost", "lme4", "hmisc", "htmlwidgets", "openxlsx", "DT",
+package_list <- c("tidyverse", "readr", "dplyr", "shiny", "shinytemplate", "plotly", "data.table",
+                  "xgboost", "lme4", "Hmisc", "htmlwidgets", "openxlsx", "DT",
                   "rmarkdown", "leaflet", "lubridate", "sf", "bit64", "IRkernel")
 
 lapply(package_list, install_packages)
-
-install_github("gatesfoundation/bmgfplot")
-install_github("gatesfoundation/gfckanr")
