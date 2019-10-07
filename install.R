a49d0b92865d440b9353a6db7773a9da1df5cb99
gnguy <grant.nguyen@gmail.com>
2019-10-07 11:44:34 -0700

    Add initial install scripts

---
+++ install.R
@@ -0,0 +1,12 @@
+install_packages <- function(package_name) {
+  if(!package_name %in% installed.packages()) install.packages(package_name)
+}
+
+package_list <- c("tidyverse", "readr", "dplyr", "shiny", "shinytemplates", "plotly", "data.table".
+                  "xgboost", "lme4", "hmisc", "htmlwidgets", "openxlsx", "DT",
+                  "rmarkdown", "leaflet", "lubridate", "sf", "bit64")
+
+lapply(package_list, install_packages)
+
+install_github("gatesfoundation/bmgfplot")
+install_github("gatesfoundation/gfckanr")
