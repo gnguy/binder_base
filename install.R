49c9c8f170a98e384c0ceb0ee4bece55b03a6832
gnguy <grant.nguyen@gmail.com>
2019-10-07 12:35:02 -0700

    Update install and add new runtime txt

--- install.R
+++ install.R
@@ -2,9 +2,9 @@ install_packages <- function(package_name) {
   if(!package_name %in% installed.packages()) install.packages(package_name)
 }
 
-package_list <- c("tidyverse", "readr", "dplyr", "shiny", "shinytemplates", "plotly", "data.table".
+package_list <- c("tidyverse", "readr", "dplyr", "shiny", "shinytemplates", "plotly", "data.table",
                   "xgboost", "lme4", "hmisc", "htmlwidgets", "openxlsx", "DT",
-                  "rmarkdown", "leaflet", "lubridate", "sf", "bit64")
+                  "rmarkdown", "leaflet", "lubridate", "sf", "bit64", "IRkernel")
 
 lapply(package_list, install_packages)
 
